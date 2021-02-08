6459d4ffa044387b9e5f4f650715bf7956f4c5c5
Evseev Oleg <oleg063053@gmail.com>
2021-02-08 16:00:29 +0400

    2. Идентичные тексты [#456411]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = false;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String res :
+                origin) {
+            check.add(res);
+        }
+        for (String resChek :
+                text) {
+            if (!check.contains(resChek)) {
+                break;
+            } else {
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+}
