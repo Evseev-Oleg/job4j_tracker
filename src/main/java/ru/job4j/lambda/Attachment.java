8a0764b7d7bf3a1dd19735a2605e84f2b9bbbdb8
Evseev Oleg <oleg063053@gmail.com>
2021-02-14 02:19:54 +0400

    1. Анонимные классы [#456220]

---
+++ src/main/java/ru/job4j/lambda/Attachment.java
@@ -0,0 +1,36 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class Attachment {
+    private String name;
+    private int size;
+
+    public Attachment(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    Comparator<String> comparator = new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.compareTo(o2);
+        }
+    };
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                '}';
+    }
+}
