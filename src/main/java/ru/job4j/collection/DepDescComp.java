f4cbafe6fbb3bf04c40cec8f5a3ec02098f9bc91
Evseev Oleg <oleg063053@gmail.com>
2021-02-14 17:19:46 +0400

    2. Отсортировать департаменты [#456387]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,18 +7,11 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String str1, String str2) {
         String[] segment1 = str1.split("/");
         String[] segment2 = str2.split("/");
-        int len = Math.min(segment1.length, segment2.length);
-        if (!segment1[0].equals(segment2[0])) {
-            return segment2[0].compareTo(segment1[0]);
-        } else if (len == 1) {
-            return Integer.compare(segment1.length, segment2.length);
-        } else {
-            for (int i = 1; i < len; i++) {
-                if (!segment1[i].equals(segment2[i])) {
-                    return segment1[i].compareTo(segment2[i]);
-                }
-            }
+        int rsl = segment2[0].compareTo(segment1[0]);
+        if(rsl == 0){
+            return str1.compareTo(str2);
+        }else{
+            return rsl;
         }
-        return 0;
     }
 }
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -20,5 +20,6 @@ public class Departments {
     }
 
     public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
     }
 }
