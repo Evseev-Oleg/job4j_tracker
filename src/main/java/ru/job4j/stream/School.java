bbf3d664c73bee7e798666a4721920c0ecaa04aa
Evseev Oleg <oleg063053@gmail.com>
2021-02-17 17:27:07 +0400

    1. Фильтрация учеников [#456230]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,8 +6,6 @@ import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> students1;
-        students1 = students.stream().filter(predict).collect(Collectors.toList());
-        return students1;
+        return students.stream().filter(predict).collect(Collectors.toList());
     }
 }
