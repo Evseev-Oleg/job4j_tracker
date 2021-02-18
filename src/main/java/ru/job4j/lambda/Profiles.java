32f9a9807398bbe2374535487c7cce03b1f5f50e
Evseev Oleg <oleg063053@gmail.com>
2021-02-18 20:41:26 +0400

    3. Уникальность элементов и сортировка. [#456232]

--- src/main/java/ru/job4j/lambda/Profiles.java
+++ src/main/java/ru/job4j/lambda/Profiles.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-    public List<Address> collect(List<Profile> profiles){
-     return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+    public List<Address> collect(List<Profile> profiles) {
+        List<Address> list;
+        list = profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        list = list.stream().sorted(new AddressByCityComparator()).distinct().collect(Collectors.toList());
+        return list;
     }
 }
--- src/test/java/ru/job4j/lambda/ProfilesTest.java
+++ src/test/java/ru/job4j/lambda/ProfilesTest.java
@@ -3,20 +3,73 @@ package ru.job4j.lambda;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
     @Test
-    public void collect() {
-        List<Profile> prof = Arrays.asList(new Profile(new Address("O"
-                , "O", 0,0)));
+    public void collectAddress() {
+        List<Profile> prof = Collections.singletonList(new Profile(new Address("O"
+                , "O", 0, 0)));
         Profiles profiles = new Profiles();
         List<Address> list = profiles.collect(prof);
         assertThat(list.get(0).getCity(), is("O"));
+    }
+
+    @Test
+    public void sortAddress() {
+        List<Address> addresses = Arrays.asList(
+                new Address("Moskow", "OOO", 0, 0),
+                new Address("Abakan", "OOO", 0, 0),
+                new Address("Baku", "OOO", 0, 0),
+                new Address("Baku", "XXX", 0, 0)
+        );
+        addresses = addresses.stream().sorted(new AddressByCityComparator())
+                .collect(Collectors.toList());
+        List<Address> excepted = Arrays.asList(
+                new Address("Abakan", "OOO", 0, 0),
+                new Address("Baku", "OOO", 0, 0),
+                new Address("Baku", "XXX", 0, 0),
+                new Address("Moskow", "OOO", 0, 0)
+        );
+        assertThat(addresses, is(excepted));
+    }
+
+    @Test
+    public void whenDistinctAddressThisNoMatches() {
+        Profiles profiles = new Profiles();
+        List<Profile> prof = Arrays.asList(
+                new Profile(new Address("Moskow", "OOO", 0, 0)),
+                new Profile(new Address("Abakan", "OOO", 0, 0)),
+                new Profile(new Address("Baku", "OOO", 0, 0)),
+                new Profile(new Address("Baku", "XXX", 0, 0)));
+        List<Address> excepted = Arrays.asList(
+                new Address("Abakan", "OOO", 0, 0),
+                new Address("Baku", "OOO", 0, 0),
+                new Address("Baku", "XXX", 0, 0),
+                new Address("Moskow", "OOO", 0, 0)
+        );
+        assertThat(profiles.collect(prof), is(excepted));
+    }
+
+    @Test
+    public void whenDistinctAddressThisOneMatches() {
+        Profiles profiles = new Profiles();
+        List<Profile> prof = Arrays.asList(
+                new Profile(new Address("Moskow", "OOO", 0, 0)),
+                new Profile(new Address("Abakan", "OOO", 0, 0)),
+                new Profile(new Address("Baku", "OOO", 0, 0)),
+                new Profile(new Address("Baku", "OOO", 0, 0)));
+        List<Address> excepted = Arrays.asList(
+                new Address("Abakan", "OOO", 0, 0),
+                new Address("Baku", "OOO", 0, 0),
+                new Address("Moskow", "OOO", 0, 0)
+        );
+        assertThat(profiles.collect(prof), is(excepted));
     }
 }
